622e69d05cd6945eef0b750b658ededbc0a5a840
Evgeniy <zhrkacomua@gmail.com>
2022-11-04 00:33:52 +0200

    start
    start

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize=require('./db')
+const PORT = process.env.PORt || 5000
+const models = require('./models/models')
+const app = express()
+
+
+
+const start = async ()=>{
+    try{
+         await sequelize.authenticate()
+         await sequelize.sync()
+        app.listen(PORT,()=>console.log(`server started on port ${PORT}`))
+    } catch (e){
+        console.log(e);
+    }
+}
+
+start()
+
+
