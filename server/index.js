d676e59b4130b562d9ce4f24e2642189ffa85e55
Evgeniy <zhrkacomua@gmail.com>
2022-11-04 23:25:04 +0200

    start:start project

--- server/index.js
+++ server/index.js
@@ -4,6 +4,13 @@ const sequelize=require('./db')
 const PORT = process.env.PORt || 5000
 const models = require('./models/models')
 const app = express()
+const cors =require('cors')
+const router = require('./routes/index')
+
+
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
 
 
 
